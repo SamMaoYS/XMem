0171c7fd03b8fcebc59bd82320a037bd043ccab6
Yongsen Mao <sammaoys@gmail.com>
2023-10-18 21:05:08 +0800

    update eval

---
+++ scripts/evaluation.py
@@ -0,0 +1,454 @@
+import json
+import argparse
+from pycocotools import mask as mask_utils
+import numpy as np
+import cv2
+import tqdm
+
+import metrics
+
+EVALMODE = "val"  # it can also be test
+
+# Threshold for the Iou Matching
+IOUTHRES = 0.1
+
+
+def reshape_img_war(img, size=(480, 480)):
+    C = 1
+    if len(img.shape) == 2:
+        H, W = img.shape
+        img = img[..., None]
+    else:
+        H, W, C = img.shape
+    temp = np.zeros((max(H, W), max(H, W), C), dtype=np.uint8)
+    if H > W:
+        L = (H - W) // 2
+        temp[:, L:-L] = img
+    elif W > H:
+        L = (W - H) // 2
+        temp[L:-L] = img
+    else:
+        temp = img
+    temp = cv2.resize(temp, size, interpolation=cv2.INTER_NEAREST)
+    return temp
+
+
+def reshape_img_nopad(img, max_dim=480):
+    H, W = img.shape[:2]
+    if H > W:
+        ratio = 1.0 / H * max_dim
+    else:
+        ratio = 1.0 / W * max_dim
+    newH, newW = int(H * ratio), int(W * ratio)
+    img = cv2.resize(img, (newW, newH), interpolation=cv2.INTER_NEAREST)
+    return img
+
+
+def remove_pad(img, orig_size):
+    cur_H, cur_W = img.shape[:2]
+    orig_H, orig_W = orig_size
+    if orig_W > orig_H:
+        ratio = 1.0 / orig_W * cur_W
+    else:
+        ratio = 1.0 / orig_H * cur_H
+    new_H, new_W = int(orig_H * ratio), int(orig_W * ratio)
+    if new_W > new_H:
+        diff_H = (cur_H - new_H) // 2
+        img = img[diff_H:-diff_H]
+    else:
+        diff_W = (cur_W - new_W) // 2
+        img = img[:, diff_W:-diff_W]
+    return img
+
+
+def getIoU(gt_mask, pred_mask):
+    gt_mask = mask_utils.decode(gt_mask)
+    gt_mask = reshape_img_war(gt_mask)
+    pred_mask = mask_utils.decode(pred_mask)
+    intersection = np.logical_and(gt_mask, pred_mask).sum()
+    union = np.logical_or(gt_mask, pred_mask).sum()
+    return intersection / union
+
+
+def eval_mask(gt_masks: np.ndarray, fake_masks: np.ndarray):
+    """TODO: Docstring for eval_mask.
+
+    Args:
+        gt_masks (np.ndarray): The
+        fake_masks (np.ndarray): TODO
+
+    Returns: TODO
+
+    """
+    iou = metrics.db_eval_iou(gt_masks, fake_masks)
+    boundary = metrics.db_eval_boundary(gt_masks, fake_masks)
+    return iou, boundary
+
+
+def existence_accuracy(gt_mask: np.ndarray, pred_mask: np.ndarray):
+    gt_zeros = np.all(gt_mask == 0)
+    pred_zeros = np.all(pred_mask == 0)
+
+    return gt_zeros == pred_zeros
+
+
+def processGTPred_EGOEXO(datapath, take_id, take_annotation, gt, pred, object_ids):
+    IoUs = []
+    ShapeAcc = []
+    ExistenceAcc = []
+    LocationScores = []
+    MDistNorm = []
+    MDistBinning = []
+
+    H, W = 480, 480  # resolution for evalution
+
+    for object_id in object_ids:
+        ego_cams = [x for x in gt["masks"][object_id].keys() if "aria" in x]
+        if len(ego_cams) < 1:
+            continue
+        assert len(ego_cams) == 1
+        EGOCAM = ego_cams[0]
+
+        EXOCAMS = [x for x in gt["masks"][object_id].keys() if "aria" not in x]
+        for exo_cam in EXOCAMS:
+            gt_masks_ego = {}
+            gt_masks_exo = {}
+            pred_masks_exo = {}
+
+            if EGOCAM in gt["masks"][object_id].keys():
+                gt_masks_ego = gt["masks"][object_id][EGOCAM]
+            if exo_cam in gt["masks"][object_id].keys():
+                gt_masks_exo = gt["masks"][object_id][exo_cam]
+            if (
+                object_id in pred["masks"].keys()
+                and f"{exo_cam}" in pred["masks"][object_id].keys()
+            ):
+                pred_masks_exo = pred["masks"][object_id][f"{exo_cam}"]
+
+            frame = cv2.imread(f"{datapath}/{take_id}/{exo_cam}/000001.jpg")
+            orig_mask_shape = frame.shape[:2]
+
+            for frame_idx in gt_masks_ego.keys():
+                if (
+                    frame_idx
+                    not in take_annotation["object_masks"][object_id][exo_cam][
+                        # "annotated_frames"
+                        "annotation"
+                    ].keys()
+                ):
+                    continue
+
+                if not frame_idx in gt_masks_exo:
+                    if orig_mask_shape[0] < orig_mask_shape[1]:
+                        gt_mask = np.zeros((270, 480), dtype=np.uint8)
+                    else:
+                        gt_mask = np.zeros((480, 270), dtype=np.uint8)
+                else:
+                    gt_mask = mask_utils.decode(gt_masks_exo[frame_idx])
+                    # reshaping without padding for evaluation
+                    gt_mask = reshape_img_nopad(gt_mask, 960)
+
+                try:
+                    if frame_idx in pred_masks_exo:
+                        pred_mask = mask_utils.decode(pred_masks_exo[frame_idx])
+                    else:
+                        pred_mask = np.zeros_like(gt_mask)
+                    # remove padding from the predictions
+                    # pred_mask = remove_pad(pred_mask, orig_size=orig_mask_shape)
+                except:
+                    breakpoint()
+
+                # iou and shape accuracy
+                try:
+                    iou, shape_acc = eval_mask(gt_mask, pred_mask)
+                except:
+                    breakpoint()
+
+                # compute existence acc i.e. if gt == pred == ALL ZEROS or gt == pred == SOME MASK
+                ex_acc = existence_accuracy(gt_mask, pred_mask)
+
+                # location accuracy as defined by Feng
+                (
+                    (gt_size, pred_size),
+                    (centroid_gt, centroid_pred),
+                    (gt_compact_mask, pred_compact_mask),
+                ) = metrics.crop_mask(gt_mask, pred_mask)
+                centroid_distance = np.sqrt(
+                    (centroid_gt[0] - centroid_pred[0]) ** 2
+                    + (centroid_gt[1] - centroid_pred[1]) ** 2
+                )
+                location_score = centroid_distance / np.sqrt(H**2 + W**2)
+
+                # MdistNorm - distance b/w biggest contour
+                mdist = metrics.getMidDistNorm(gt_mask, pred_mask)
+                if mdist != -1:
+                    MDistNorm.append(mdist)
+
+                mdistbin = metrics.getMidBinning(gt_mask, pred_mask)
+                if mdistbin != -1:
+                    MDistBinning.append(mdistbin)
+
+                IoUs.append(iou)
+                ShapeAcc.append(shape_acc)
+                ExistenceAcc.append(ex_acc)
+                LocationScores.append(location_score)
+
+    IoUs = np.array(IoUs)
+    ShapeAcc = np.array(ShapeAcc)
+    ExistenceAcc = np.array(ExistenceAcc)
+    LocationScores = np.array(LocationScores)
+    MDistNorm = np.array(MDistNorm)
+    MDistBinning = np.array(MDistBinning)
+
+    # print(f"ego_exo: Total {ego_exo.shape[0]}, Correct {np.sum(ego_exo)}, Accuracy {np.mean(ego_exo)}")
+    # print(f"ego_noexo: Total {ego_noexo.shape[0]}, Correct {np.sum(ego_noexo)}, Accuracy {np.mean(ego_noexo)}")
+
+    # print("ego_exo IoU: ", np.mean(IoUs))
+    # print("ShapeAcc: ", np.mean(ShapeAcc))
+    # print("ExistenceAcc: ", np.mean(ExistenceAcc))
+    # if np.mean(IoUs) == 0:
+    #     breakpoint()
+
+    return (
+        IoUs.tolist(),
+        ShapeAcc.tolist(),
+        ExistenceAcc.tolist(),
+        LocationScores.tolist(),
+        MDistNorm.tolist(),
+        MDistBinning.tolist(),
+    )  # list(ego_exo), list(ego_noexo)
+
+
+def processGTPred_EXOEGO(take_annotation, gt, pred, object_ids):
+    IoUs = []
+    ShapeAcc = []
+    ExistenceAcc = []
+    LocationScores = []
+    MDistNorm = []
+    MDistBinning = []
+
+    H, W = 480, 480  # resolution for evalution
+
+    for object_id in object_ids:
+        ego_cams = [x for x in gt["masks"][object_id].keys() if "aria" in x]
+        if len(ego_cams) < 1:
+            continue
+        assert len(ego_cams) == 1
+        EGOCAM = ego_cams[0]
+
+        EXOCAMS = [x for x in gt["masks"][object_id].keys() if "aria" not in x]
+        for exo_cam in EXOCAMS:
+            gt_masks_ego = {}
+            gt_masks_exo = {}
+            pred_masks_ego = {}
+
+            if EGOCAM in gt["masks"][object_id].keys():
+                gt_masks_ego = gt["masks"][object_id][EGOCAM]
+            if exo_cam in gt["masks"][object_id].keys():
+                gt_masks_exo = gt["masks"][object_id][exo_cam]
+            if (
+                object_id in pred["masks"].keys()
+                and f"{exo_cam}_{EGOCAM}" in pred["masks"][object_id].keys()
+            ):
+                pred_masks_ego = pred["masks"][object_id][f"{exo_cam}_{EGOCAM}"]
+
+            for frame_idx in gt_masks_exo.keys():
+                if (
+                    int(frame_idx)
+                    not in take_annotation["object_masks"][object_id][EGOCAM][
+                        "annotated_frames"
+                    ]
+                ):
+                    continue
+
+                if not frame_idx in gt_masks_ego:
+                    gt_mask = np.zeros((H, W), dtype=np.uint8)
+                else:
+                    gt_mask = mask_utils.decode(gt_masks_ego[frame_idx])
+                    gt_mask = reshape_img_nopad(gt_mask)
+
+                # breakpoint()
+                pred_mask = mask_utils.decode(pred_masks_ego[frame_idx])
+
+                # iou and shape accuracy
+                iou, shape_acc = eval_mask(gt_mask, pred_mask)
+
+                # compute existence acc i.e. if gt == pred == ALL ZEROS or gt == pred == SOME MASK
+                ex_acc = existence_accuracy(gt_mask, pred_mask)
+
+                # location accuracy as defined by Feng
+                (
+                    (gt_size, pred_size),
+                    (centroid_gt, centroid_pred),
+                    (gt_compact_mask, pred_compact_mask),
+                ) = metrics.crop_mask(gt_mask, pred_mask)
+                centroid_distance = np.sqrt(
+                    (centroid_gt[0] - centroid_pred[0]) ** 2
+                    + (centroid_gt[1] - centroid_pred[1]) ** 2
+                )
+                location_score = centroid_distance / np.sqrt(H**2 + W**2)
+
+                # MdistNorm - distance b/w biggest contour
+                mdist = metrics.getMidDistNorm(gt_mask, pred_mask)
+                if mdist != -1:
+                    MDistNorm.append(mdist)
+
+                mdistbin = metrics.getMidBinning(gt_mask, pred_mask)
+                if mdistbin != -1:
+                    MDistBinning.append(mdistbin)
+
+                IoUs.append(iou)
+                ShapeAcc.append(shape_acc)
+                ExistenceAcc.append(ex_acc)
+                LocationScores.append(location_score)
+
+    IoUs = np.array(IoUs)
+    ShapeAcc = np.array(ShapeAcc)
+    ExistenceAcc = np.array(ExistenceAcc)
+    LocationScores = np.array(LocationScores)
+    MDistNorm = np.array(MDistNorm)
+    MDistBinning = np.array(MDistBinning)
+
+    # print(f"ego_exo: Total {ego_exo.shape[0]}, Correct {np.sum(ego_exo)}, Accuracy {np.mean(ego_exo)}")
+    # print(f"ego_noexo: Total {ego_noexo.shape[0]}, Correct {np.sum(ego_noexo)}, Accuracy {np.mean(ego_noexo)}")
+
+    # print("ego_exo IoU: ", np.mean(IoUs))
+    # print("ShapeAcc: ", np.mean(ShapeAcc))
+    # print("ExistenceAcc: ", np.mean(ExistenceAcc))
+    # if np.mean(IoUs) == 0:
+    #     breakpoint()
+
+    return (
+        IoUs.tolist(),
+        ShapeAcc.tolist(),
+        ExistenceAcc.tolist(),
+        LocationScores.tolist(),
+        MDistNorm.tolist(),
+        MDistBinning.tolist(),
+    )  # list(ego_exo), list(ego_noexo)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--datapath",
+        help="the correspondence dataset path",
+        default="/Users/shawn/Desktop/proj-egoexo/correspondence_dataset/dataset_correspondence",
+    )
+    parser.add_argument(
+        "--inference_path",
+        help="The predicted path",
+        default="/Users/shawn/Desktop/proj-egoexo/correspondence_dataset/dataset_correspondence",
+    )
+    parser.add_argument(
+        "--reversed", help="if true: do Exo->Ego else Ego->Exo", action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    split_file = f"{args.datapath}/split.json"
+    with open(split_file, "r") as f:
+        split = json.load(f)
+        test_ids = split[EVALMODE]
+
+    annotations_file = f"{args.datapath}/relations_objects_latest.json"
+    with open(annotations_file, "r") as fp:
+        annotations = json.load(fp)
+
+    total_iou = []
+    total_shape_acc = []
+    total_existence_acc = []
+    total_location_scores = []
+    total_mdistnorm_scores = []
+    total_mdistbin_scores = []
+
+    take_metrics = {}
+
+    for take_id in tqdm.tqdm(test_ids):
+        take_id = take_id[0]
+        print(f"Processing take {take_id}")
+        # Load the GT annotations
+        gt_file = f"{args.datapath}/{take_id}/annotation.json"
+        with open(gt_file, "r") as f:
+            gt = json.load(f)
+        # Load the predictions
+        pred_file = f"{args.inference_path}/{take_id}/pred_annotations.json"
+        with open(pred_file, "r") as f:
+            pred = json.load(f)
+
+        object_ids = list(gt["masks"].keys())
+
+        if args.reversed:
+            (
+                ious,
+                shape_accs,
+                existence_accs,
+                location_scores,
+                mdistnorm_scores,
+                mdistbin_scores,
+            ) = processGTPred_EXOEGO(annotations[take_id], gt, pred, object_ids)
+        else:
+            (
+                ious,
+                shape_accs,
+                existence_accs,
+                location_scores,
+                mdistnorm_scores,
+                mdistbin_scores,
+            ) = processGTPred_EGOEXO(
+                args.datapath, take_id, annotations[take_id], gt, pred, object_ids
+            )
+        total_iou += ious
+        total_shape_acc += shape_accs
+        total_existence_acc += existence_accs
+        total_location_scores += location_scores
+        total_mdistnorm_scores += mdistnorm_scores
+        total_mdistbin_scores += mdistbin_scores
+
+        take_metrics[take_id] = {
+            "iou": np.mean(ious),
+            "shape_accs": np.mean(shape_accs),
+            "existence_acc": np.mean(existence_accs),
+            "location_score": np.mean(location_scores),
+            "mdistnorm_score": np.mean(mdistnorm_scores),
+            "mdistbin_score": np.mean(mdistbin_scores),
+        }
+        print("\n")
+
+    # print(f"Total ego_exo: Total {len(total_ego_exo)}, Correct {np.sum(total_ego_exo)}, Accuracy {np.mean(total_ego_exo)}")
+    # print(f"Total ego_noexo: Total {len(total_ego_noexo)}, Correct {np.sum(total_ego_noexo)}, Accuracy {np.mean(total_ego_noexo)}")
+    print("TOTAL IOU: ", len(total_iou), np.mean(total_iou))
+    print("TOTAL SHAPE ACC: ", len(total_shape_acc), np.mean(total_shape_acc))
+    print(
+        "TOTAL EXISTENCE ACC: ", len(total_existence_acc), np.mean(total_existence_acc)
+    )
+    print(
+        "TOTAL LOCATION SCORE: ",
+        len(total_location_scores),
+        np.mean(total_location_scores),
+    )
+    print(
+        "TOTAL MDISTNORM SCORE: ",
+        len(total_mdistnorm_scores),
+        np.mean(total_mdistnorm_scores),
+    )
+    print(
+        "TOTAL MDISTBIN SCORES: ",
+        len(total_mdistbin_scores),
+        np.mean(total_mdistbin_scores),
+    )
+
+    with open(f"{args.inference_path}/take_metrics_{EVALMODE}.json", "w") as fp:
+        json.dump(take_metrics, fp)
+
+    total_metrics = {
+        "iou": total_iou,
+        "shape_accs": total_shape_acc,
+        "existence_acc": total_existence_acc,
+        "location_score": total_location_scores,
+        "mdistnorm_score": total_mdistbin_scores,
+        "mdistbin_score": total_mdistbin_scores,
+    }
+
+    with open(f"{args.inference_path}/total_metrics_{EVALMODE}.json", "w") as fp:
+        json.dump(total_metrics, fp)
---
+++ scripts/merge_pred.py
@@ -0,0 +1,77 @@
+import os
+import argparse
+import json
+
+from tqdm.auto import tqdm
+
+
+def main(args):
+    split_data = None
+    splits_path = args.split_json
+    with open(splits_path, "r") as fp:
+        split_data = json.load(fp)
+
+    if split_data is None:
+        print("No split found")
+        return
+
+    takes = [take_id[0] for take_id in split_data[args.split]]
+    for take_id in tqdm(takes):
+        result = process_take(take_id, args.input, args.pred)
+
+        with open(
+            os.path.join(args.pred, take_id, "pred_annotations.json"), "w+"
+        ) as fp:
+            json.dump(result, fp)
+
+
+def process_take(take_id, input, pred):
+    annotation_path = os.path.join(input, take_id, "annotation.json")
+    with open(annotation_path, "r") as fp:
+        annotation = json.load(fp)
+    masks = annotation["masks"]
+
+    pred_masks = {}
+    for object_name, cams in masks.items():
+        pred_masks[object_name] = {}
+        for cam_name, cam_data in cams.items():
+            if not os.path.isdir(os.path.join(input, take_id, cam_name)):
+                continue
+
+            pred_masks[object_name][cam_name] = {}
+
+            frames = list(cam_data.keys())
+            for f_name in frames:
+                f_str = "{:06d}".format(int(int(f_name) / 30 + 1))
+
+                pred_mask_path = os.path.join(
+                    pred, take_id, cam_name, object_name, f_str + ".json"
+                )
+                if not os.path.isfile(pred_mask_path):
+                    continue
+
+                with open(pred_mask_path, "r") as fp:
+                    pred_mask_data = json.load(fp)
+                pred_masks[object_name][cam_name][f_name] = pred_mask_data
+    return {"masks": pred_masks}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input",
+        help="EgoExo take data root",
+        default="../data/correspondence",
+    )
+    parser.add_argument(
+        "--split_json",
+        help="EgoExo take data root",
+        default="../data/correspondence/split.json",
+    )
+    parser.add_argument("--split", help="EgoExo take data root", default="val")
+    parser.add_argument(
+        "--pred", help="EgoExo take data root", default="../output/E23_val/Annotations"
+    )
+    args = parser.parse_args()
+
+    main(args)
