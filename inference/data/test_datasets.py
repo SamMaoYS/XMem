150fa9613ac5612dc436576f6a2fa336b1d74821
Yongsen Mao <sammaoys@gmail.com>
2023-10-24 20:34:12 +0800

    update eval

--- inference/data/test_datasets.py
+++ inference/data/test_datasets.py
@@ -166,9 +166,9 @@ class EgoExoTestDataset:
             yield EgoExoVideoReader(
                 os.path.join(take, exo_cam_name, obj),
                 path.join(self.data_root, take, exo_cam_name, obj),
-                path.join(self.data_root, take, self.ego_cam_name, obj),
+                path.join(self.data_root, take, ego_cam_name, obj),
                 size=self.size,
-                to_save=self.req_frame_list[os.path.join(take, exo_cam_name, obj)],
+                to_save=self.req_frame_list[video],
                 use_all_mask=False,
             )
 
