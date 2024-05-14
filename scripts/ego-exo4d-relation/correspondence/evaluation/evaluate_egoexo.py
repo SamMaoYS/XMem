f2b8daac44ad67b8259ee83f4973bd560832e6dc
Yongsen Mao <sammaoys@gmail.com>
2024-05-14 22:33:52 +0800

    update

--- scripts/ego-exo4d-relation/correspondence/evaluation/evaluate_egoexo.py
+++ scripts/ego-exo4d-relation/correspondence/evaluation/evaluate_egoexo.py
@@ -65,7 +65,10 @@ def evaluate_take(gt, pred):
                     gt_obj_exists = 1
 
                 try:
-                    pred_mask = mask_utils.decode(pred_masks_exo[frame_idx])
+                    if pred_masks_exo.get(frame_idx, None) is None:
+                        pred_mask = np.zeros_like(gt_mask)
+                    else:
+                        pred_mask = mask_utils.decode(pred_masks_exo[frame_idx])
                     # remove padding from the predictions
                     # # TODO: remove from here: move to inference script
                     # if not gt_mask is None:
