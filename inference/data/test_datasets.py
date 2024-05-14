1a2227945594fdd22ba65121e56ec0815902db34
Yongsen Mao <sammaoys@gmail.com>
2024-05-14 16:16:21 +0800

    update

--- inference/data/test_datasets.py
+++ inference/data/test_datasets.py
@@ -46,11 +46,7 @@ class EgoExoTestDataset:
 
                     if swap:
                         ego_frames = natsorted(
-                            os.listdir(
-                                path.join(
-                                    self.data_root, take_id, ego_cam_name, object_name
-                                )
-                            )
+                            os.listdir(path.join(self.data_root, take_id, ego_cam_name))
                         )
                         ego_frames = [int(f.split(".")[0]) for f in ego_frames]
                         frames = np.intersect1d(ego_frames, exo_frames)
@@ -64,11 +60,7 @@ class EgoExoTestDataset:
                             )
                     else:
                         exo_frames = natsorted(
-                            os.listdir(
-                                path.join(
-                                    self.data_root, take_id, cam_name, object_name
-                                )
-                            )
+                            os.listdir(path.join(self.data_root, take_id, cam_name))
                         )
                         exo_frames = [int(f.split(".")[0]) for f in exo_frames]
                         frames = np.intersect1d(ego_frames, exo_frames)
