90116046d0ffcdcb913d7f1fa2f7d23d691574d6
Yongsen Mao <sammaoys@gmail.com>
2024-05-14 22:42:53 +0800

    update

--- scripts/ego-exo4d-relation/correspondence/evaluation/evaluate_egoexo.py
+++ scripts/ego-exo4d-relation/correspondence/evaluation/evaluate_egoexo.py
@@ -76,9 +76,9 @@ def evaluate_take(gt, pred):
                 except:
                     breakpoint()
 
-                pred_obj_exists = int(
-                    pred_masks_exo[frame_idx].get("confidence", 1) > CONF_THRESH
-                )
+                # pred_obj_exists = int(
+                #     pred_masks_exo[frame_idx].get("confidence", 1) > CONF_THRESH
+                # )
                 pred_obj_exists = (
                     np.sum(pred_mask) > pred_mask.shape[0] * pred_mask.shape[1] * 0.001
                 )
